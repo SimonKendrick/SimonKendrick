a85c8bb4b9fb63f6d8cbe4444fe17a05eb05ee1d
Simon Kendrick <Simon.Kendrick@ad.unsw.edu.au>
2024-05-21 15:24:09 +1000

    pages

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 
 
 
+//console.log(document.querySelector("a[href='#new-intervention']"));
+
+
+
+const handleAboutClick = (event) => {
+
+    preventDefault();
+
+
+    console.log(aboutAnchorElement);
+}
+
+
+
+let aboutAnchorElement=document.querySelector("a[href='about']");
+
+
+aboutAnchorElement.addEventListener('onclick',handleAboutClick);
+
+
