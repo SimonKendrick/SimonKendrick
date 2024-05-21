ee21a48ae987131493a619d646592a5dcc27cb05
Simon Kendrick <Simon.Kendrick@ad.unsw.edu.au>
2024-05-21 16:58:22 +1000

    design

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
-
-
-
-//console.log(document.querySelector("a[href='#new-intervention']"));
-
-
-
 const handleAboutClick = (event) => {
 
-    preventDefault();
+    event.preventDefault();
 
+    aboutPageElement.style.display="flex";
+    travelPageElement.style.display="none";
+    hackingPageElement.style.display="none";
 
     console.log(aboutAnchorElement);
+
 }
 
+const handleTravelClick = (event) => {
+
+    event.preventDefault();
+
+    aboutPageElement.style.display="none";
+    travelPageElement.style.display="flex";
+    hackingPageElement.style.display="none";
+
+    console.log(aboutAnchorElement);
+
+}
+const handleHackingClick = (event) => {
+
+    event.preventDefault();
+
+    aboutPageElement.style.display="none";
+    travelPageElement.style.display="none";
+    hackingPageElement.style.display="flex";
+
+    console.log(aboutAnchorElement);
+
+}
+
+let aboutAnchorElement=document.getElementsByName("about_anchor")[0];
+aboutAnchorElement.addEventListener("click",handleAboutClick,false);
+
+let travelAnchorElement=document.getElementsByName("travel_anchor")[0];
+travelAnchorElement.addEventListener("click",handleTravelClick,false);
+
+let hackingAnchorElement=document.getElementsByName("hacking_anchor")[0];
+hackingAnchorElement.addEventListener("click",handleHackingClick,false);
 
 
-let aboutAnchorElement=document.querySelector("a[href='about']");
 
+let aboutPageElement=document.getElementsByClassName("about_page")[0];
 
-aboutAnchorElement.addEventListener('onclick',handleAboutClick);
+let travelPageElement=document.getElementsByClassName("travel_page")[0];
+
+let hackingPageElement=document.getElementsByClassName("hacking_page")[0];
+
 
 
