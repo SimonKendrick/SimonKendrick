1473da8aeedbb9fa7c00d44f607eb7868b36a8e4
Simon Kendrick <Simon.Kendrick@ad.unsw.edu.au>
2024-05-24 09:24:13 +1000

    about height

--- index.js
+++ index.js
@@ -1,82 +1,40 @@
-
-const reset = (event) => {
-
+var reset = function (event) {
     event.preventDefault();
-
-    aboutPageElement.style.display="none";
-    travelPageElement.style.display="none";
-    hackingPageElement.style.display="none";
-    landingPageElement.style.display="none";
-    
+    aboutPageElement.style.display = "none";
+    travelPageElement.style.display = "none";
+    hackingPageElement.style.display = "none";
+    landingPageElement.style.display = "none";
     contentElement.style.backgroundImage = "url()";
     contentElement.style.backgroundImage = "linear-gradient(to bottom, rgb(0 0 255 / 50%), rgb(255 255 0 / 50%))";
-
-
-}
-
-
-
-const handleAboutClick = (event) => {
-
+};
+var handleAboutClick = function (event) {
     reset(event);
-
-
-    aboutPageElement.style.display="flex";
-
-
-
+    aboutPageElement.style.display = "flex";
     console.log(aboutAnchorElement);
-
-}
-
-const handleTravelClick = (event) => {
-
+};
+var handleTravelClick = function (event) {
     reset(event);
-
-
-
-    travelPageElement.style.display="flex";
-
-
+    travelPageElement.style.display = "flex";
     console.log(aboutAnchorElement);
-
-}
-const handleHackingClick = (event) => {
-
-
+};
+var handleHackingClick = function (event) {
     reset(event);
-
-
     event.preventDefault();
-
-
-    hackingPageElement.style.display="flex";
-
-    
+    hackingPageElement.style.display = "flex";
     console.log(aboutAnchorElement);
-
-}
-
-let aboutAnchorElement=document.getElementsByName("about_anchor")[0];
-aboutAnchorElement.addEventListener("click",handleAboutClick,false);
-
-let travelAnchorElement=document.getElementsByName("travel_anchor")[0];
-travelAnchorElement.addEventListener("click",handleTravelClick,false);
-
-let hackingAnchorElement=document.getElementsByName("hacking_anchor")[0];
-hackingAnchorElement.addEventListener("click",handleHackingClick,false);
-
-
-
-let contentElement=document.getElementsByClassName("content")[0];
-
-let landingPageElement=document.getElementsByClassName("landing_page")[0];
-
-let aboutPageElement=document.getElementsByClassName("about_page")[0];
-
-let travelPageElement=document.getElementsByClassName("travel_page")[0];
-
-let hackingPageElement=document.getElementsByClassName("hacking_page")[0];
-
-
-
+};
+var aboutAnchorElement = document.getElementsByName("about_anchor")[0];
+aboutAnchorElement.addEventListener("click", handleAboutClick, false);
+var travelAnchorElement = document.getElementsByName("travel_anchor")[0];
+travelAnchorElement.addEventListener("click", handleTravelClick, false);
+var hackingAnchorElement = document.getElementsByName("hacking_anchor")[0];
+hackingAnchorElement.addEventListener("click", handleHackingClick, false);
+var contentElement = document.getElementsByClassName("content")[0];
+;
+var landingPageElement = document.getElementsByClassName("landing_page")[0];
+;
+var aboutPageElement = document.getElementsByClassName("about_page")[0];
+var travelPageElement = (document.getElementsByClassName("travel_page")[0]);
+;
+var hackingPageElement = document.getElementsByClassName("hacking_page")[0];
+;
